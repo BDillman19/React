ea741e19e66c49ee73b9f79b579d9d2068256280
Bryce Dillman <dillman19199@gmail.com>
2019-09-17 15:01:28 -0500

    working on components and scaffolding seasons project

--- components/src/index.js
+++ components/src/index.js
@@ -2,30 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import Faker from 'faker';
+import ApprovalCard from './ApprovalCode';
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <CommentDetail 
-                author="sam" 
-                time="6:00PM" 
-                message="Nice blog post" 
-                image={Faker.image.avatar()}/>
-            <CommentDetail 
-                author="bryce" 
-                time="6:52PM" 
-                message="informative" 
-                image={Faker.image.avatar()}/>
-            <CommentDetail 
-                author="kayla" 
-                time="7:20PM" 
-                message="interesting points" 
-                image={Faker.image.avatar()}/>
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
         </div>
-    );
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="sam" 
+          time="6:00PM" 
+          message="Nice blog post" 
+          image={Faker.image.avatar()}/>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="bryce" 
+          time="6:52PM" 
+          message="informative" 
+          image={Faker.image.avatar()}/>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="kayla" 
+          time="7:20PM" 
+          message="interesting points" 
+          image={Faker.image.avatar()}/>
+      </ApprovalCard>
+    </div>
+  );
 }
 
 ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
+  <App />,
+  document.querySelector('#root')
 );
---
+++ seasons/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>All set</div>
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
