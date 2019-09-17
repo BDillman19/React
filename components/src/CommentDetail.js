ad54a7e0d7d295f6c0df612dc5db2baf56564088
Bryce Dillman <dillman19199@gmail.com>
2019-09-16 23:53:10 -0500

    more testing with react

---
+++ components/src/CommentDetail.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const CommentDetail = (prop) => {
+    return (
+        <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={props.image}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        {props.author}
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at {props.time}</span>
+                    </div>
+                    <div className="text">{props.message}</div>
+                </div>
+            </div>
+    )
+}
+
+export default CommentDetail;
--- components/src/index.js
+++ components/src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import CommentDetail from './CommentDetail';
+import Faker from 'faker';
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <CommentDetail 
+                author="sam" 
+                time="6:00PM" 
+                message="Nice blog post" 
+                image={Faker.image.avatar()}/>
+            <CommentDetail 
+                author="bryce" 
+                time="6:52PM" 
+                message="informative" 
+                image={Faker.image.avatar()}/>
+            <CommentDetail 
+                author="kayla" 
+                time="7:20PM" 
+                message="interesting points" 
+                image={Faker.image.avatar()}/>
+        </div>
+    );
+}
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+    document.querySelector('#root')
 );
